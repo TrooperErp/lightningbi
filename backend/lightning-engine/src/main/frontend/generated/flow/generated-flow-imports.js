cc245d27f16f6f1db3c08036f655fe01c7abf6a3
AndreaZallocco84 <a.zallocco84@gmail.com>
2026-09-18 20:02:01 +0200

    invia

--- backend/lightning-engine/src/main/frontend/generated/flow/generated-flow-imports.js
+++ backend/lightning-engine/src/main/frontend/generated/flow/generated-flow-imports.js
@@ -5,11 +5,11 @@ import 'react';
 
 const loadOnDemand = (key) => {
   const pending = [];
-  if (key === '20177c7fbddc3cbb2ad8a9de8f440f79cb12c66b35bf4a53949539ad577e04da') {
-    pending.push(import('./chunks/chunk-06cfe298f2b72c1aeefe361c3c22a00e93b92731f66bd62f2b03168f97b2e45e.js'));
-  }
   if (key === '6200366fed7c7273360acb3003ed785a36114178aa190b855ed43428396b8f51') {
     pending.push(import('./chunks/chunk-b867a353ce45039e53daebbd2c9b19e4443a267ad654f5a27fedf5a2b550a7f3.js'));
+  }
+  if (key === '20177c7fbddc3cbb2ad8a9de8f440f79cb12c66b35bf4a53949539ad577e04da') {
+    pending.push(import('./chunks/chunk-06cfe298f2b72c1aeefe361c3c22a00e93b92731f66bd62f2b03168f97b2e45e.js'));
   }
   return Promise.all(pending);
 }
